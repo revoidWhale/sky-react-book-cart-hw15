90278537595d9993de9c55b44d2f5aa7bd86155f
Evgeniy Eremin <103576500+revoidWhale@users.noreply.github.com>
2022-06-12 14:51:25 +0700

    Initial commit

---
+++ src/component/Title.js
@@ -0,0 +1,12 @@
+function Title(props) {
+  return (
+    <div className="card-title">
+      <div className="card-title-top">
+        <input type="text" onChange />
+      </div>
+      <p>{}</p>
+    </div>
+  )
+}
+
+export default Title
